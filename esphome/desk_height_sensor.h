4454ad0f29949c08c05aa87a8866b83bd1929ac6
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2022-12-18 05:16:56 +0100

    [esphome/desk-control] Fix my buggy code

--- esphome/desk_height_sensor.h
+++ esphome/desk_height_sensor.h
@@ -8,19 +8,26 @@ public:
   void setup() override {}
 
   void loop() override {
+    // Desk controller frames are 3 bytes long
     uint8_t sequence[this->FRAME_LENGTH] = {0};
-    uint8_t idx = -1;
+    uint8_t idx = 0;
     uint8_t data;
+
     while (available()) {
       data = read();
 
-      if (data > -1)
-        sequence[++idx] = data;
+      if (data > -1) {
+        sequence[idx] = data;
+        ++idx;
+      }
 
-      if (*(uint16_t*)(sequence) == 1 and idx == this->FRAME_LENGTH)
-        publish_state(sequence[2]);
+      if (idx == this->FRAME_LENGTH)
+        break;
     }
-  }
+
+    if (*(uint16_t*)sequence == 1 and idx == this->FRAME_LENGTH)
+      publish_state(sequence[2]);
+ }
 private:
   const uint8_t FRAME_LENGTH = 3;
 }; // DeskHeightSensor
